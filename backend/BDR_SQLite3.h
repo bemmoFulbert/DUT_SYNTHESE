aafd75561f6ba6dc89f67b55831b6bcef744d41e
jord <jordanpeggy01@gmail.com>
2022-07-10 22:33:28 +0100

    added folder 'backend' containing WeakSQLCommandGenerator.h and BDR_SQLite3.h c++ classes

---
+++ backend/BDR_SQLite3.h
@@ -0,0 +1,152 @@
+#ifndef BDR_SQLITE_H_INCLUDED
+#define BDR_SQLITE_H_INCLUDED
+
+#include "WeakSQLCommandGenerator.h"
+#include "sqlite3.h"
+
+
+
+class BDR_SQLite3{
+    private:
+        string filename;
+        sqlite3 *db_handle;
+        bool is_open;
+    public:
+        BDR_SQLite3(const string &nomFichierBD){
+            filename = nomFichierBD;
+            is_open = ((sqlite3_open(nomFichierBD.c_str(),&db_handle) == SQLITE_OK) ? true : false);
+            if (!is_open){
+                cerr << "BDR_SQLite:: " << this << " n'a pas pu être ouvert" << endl;
+            }
+        };
+        ~BDR_SQLite3(){
+            sqlite3_close(db_handle);
+        };
+
+    public:
+        bool estOuvert(){
+            return is_open;
+        };
+
+        sqlite3* recupererObjetSQLite3(){
+            return db_handle;
+        };
+
+        string& recupererNomFichierBD(){
+            return filename;
+        };
+
+
+    public:
+        /*
+            A noter que  (valeurs.size() / nomChamps.size()) doit être le nombre de tuples a ajouter
+        */
+        bool ajouter(const string &nomTable,const vector<string> &nomChamps,const vector<string> &valeurs){
+            /*double l = (double)valeurs.size()/(double)nomChamps.size();
+            if (l != (long long)l){
+                cerr << "*Warning* un tuple de valeurs incomplet a éte detecter sur une appelle de la fonction TableBDR::ajouter" << endl;
+            }*/
+
+            char *err_msg;
+            if (sqlite3_exec(db_handle,WeakSQLCommandGenerator::Insert(nomTable,nomChamps,valeurs).c_str(),NULL,NULL,&err_msg)){
+                cerr << "*Fatale* BDR_SQLite::ajouter : " << err_msg << endl;
+                sqlite3_free(err_msg);
+                return false;
+            }
+            return true;
+        }
+
+
+        /*
+            A noter que nouvelValeurs.size() == nomChamps.size()
+
+            "UPDATE nomTable SET .... WHERE (" + cond + ")"  , si (cond != "")
+            "UPDATE nomTable SET ...."  , si (cond == "")
+        */
+        bool modifier(const string &nomTable,const vector<string> &nomChamps,const vector<string> &nouvelValeurs,const string &cond=""){
+            char *err_msg;
+            if (sqlite3_exec(db_handle,WeakSQLCommandGenerator::Update(nomTable,nomChamps,nouvelValeurs,cond).c_str(),NULL,NULL,&err_msg)){
+                cerr << "*Fatale* BDR_SQLite::modifier : " << err_msg << endl;
+                sqlite3_free(err_msg);
+                return false;
+            }
+            return true;
+        };
+
+
+        /*
+            A noter que nouvelValeurs.size() == ids.size()*nomChamps.size()
+        */
+        bool modifier(const string &nomTable,const vector<unsigned int> &ids,const vector<string> &nomChamps,const vector<string> &nouvelValeurs,const string &cond="",const string &nomChampID="id"){
+            string condID = "";
+
+            char id_str[512];
+            for (unsigned int i = 0 ; i < ids.size() ; i++){
+                sprintf(id_str,"%u",ids[i]);
+                condID += nomChampID + "=" + id_str + ((i == ids.size()-1) ? "" : " OR ");
+            }
+
+            return modifier(nomTable,nomChamps,nouvelValeurs,condID + " AND (" + cond + ")");
+        };
+
+
+        /*
+            "DELETE FROM nomTable WHERE " + cond , si (cond != "")
+            "DELETE FROM nomTable" , si (cond == "")
+        */
+        //Supprime les tuples en se basant sur des conditions
+        bool supprimer(const string &nomTable,const string &cond=""){
+            char *err_msg;
+            if (sqlite3_exec(db_handle,WeakSQLCommandGenerator::Delete(nomTable,cond).c_str(),NULL,NULL,&err_msg) != 0){
+                cerr << "*Fatale* BDR_SQLite::supprimer : " << err_msg << endl;
+                sqlite3_free(err_msg);
+                return false;
+            }
+            return true;
+        };
+
+        //Supprime les tuples avec les ids donner
+        bool supprimer(const string &nomTable,const vector<unsigned int> &ids,const string &nomChampID="id"){
+            string condID = "";
+
+            char id_str[512];
+            for (unsigned int i = 0 ; i < ids.size() ; i++){
+                sprintf(id_str,"%u",ids[i]);
+                condID += nomChampID + "=" + id_str + ((i == ids.size()-1) ? "" : " OR ");
+            }
+            return supprimer(nomTable,condID);
+        };
+
+
+        /* Le resultat sera charger dans le vecteur *valeurs* , concat est le code SQL a ajouter devant le "SELECT **** FROM nomTable " */
+        //Consulte uniquement les champs specifier dans le vecteur nomChamps
+
+        bool consulter(const string &nomTable,const vector<string> &nomChamps,vector<string> &valeurs,const string &concat=""){
+            char *err_msg;
+            if (sqlite3_exec(db_handle,WeakSQLCommandGenerator::SelectFromOneTable(nomTable,nomChamps,concat).c_str(),consulter_cb,(void*)&valeurs,&err_msg) != 0){
+                cerr << "*Fatale* BDR_SQLite::consulter : " << err_msg << endl;
+                sqlite3_free(err_msg);
+                return false;
+            }
+            return true;
+        };
+
+        //Consulte tout les champs
+        bool consulter(const string &nomTable,vector<string> &valeurs,const string &concat=""){
+            return consulter(nomTable,{"*"},valeurs,concat);
+        };
+
+    private:
+        static int consulter_cb(void *userdata,int nb_columns,char **row,char **header_row){
+            vector<string> *vals = (vector<string>*)userdata;
+
+            for (int i = 0 ; i < nb_columns ; i++){
+                vals->push_back(string(row[i]));
+            }
+
+            return 0;
+        };
+
+};
+
+#endif // BDR_SQLITE_H_INCLUDED
---
+++ backend/WeakSQLCommandGenerator.h
@@ -0,0 +1,205 @@
+#ifndef WEAKSQLCOMMANDGENERATOR_H_INCLUDED
+#define WEAKSQLCOMMANDGENERATOR_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+/*
+    This class provides a set of methods that generates weak sql commands from userdata
+    The *weak* here means the expected sql strings passed by the user are not checked by an sql parser
+*/
+class WeakSQLCommandGenerator{
+    /*
+        - Could add *delimiter or statement_terminator* argument
+        - Could add enableLogging/disableLogging
+    */
+
+    public:
+
+        static string Insert(const string &tableName,const vector<string> &fieldNames,const vector<string> &values){
+            double l = (double)values.size()/(double)fieldNames.size();
+            if (l != (long long)l){
+                cerr << "*Warning* function WeakSQLCommandGenerator::Insert : the number of values divided by the number of fieldNames must not be a fraction or an incomplete tuple shall be encountered" << endl;
+            }
+            if (!fieldNames.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::Insert : the fieldNames array must not be empty" << endl;
+            }
+
+            string sql_com = "";
+
+            unsigned int num_columns = fieldNames.size();
+            unsigned int num_rows = values.size()/fieldNames.size();
+
+            string headers = "";
+            for (unsigned int i = 0 ; i < num_columns ; i++){
+                headers += fieldNames[i] + ",";
+            }
+
+            for (unsigned int k = 0 ; k < num_rows ; k++){
+
+                sql_com += "INSERT INTO " + tableName + "(";
+
+                sql_com += headers;
+                sql_com[sql_com.size() - 1] = ')';
+                sql_com += " VALUES(";
+
+                string vals = "";
+                for (unsigned int i = k*num_columns , stop = i + num_columns ; i < stop ; i++){
+                    vals += values[i] + ",";
+                }
+                sql_com += vals;
+                sql_com[sql_com.size() - 1] = ')';
+                sql_com += ";";
+            }
+
+            return sql_com;
+        };
+
+        //*cond* here is an sql statement appended after the WHERE keyword , it must not include the semicolon and the main surrounding brackets
+        static string Update(const string &tableName,const vector<string> &fieldNames,const vector<string> &newValues,const string &cond=""){
+            if (fieldNames.size() != newValues.size()){
+                cerr << "*Warning* function WeakSQLCommandGenerator::Update : the number of fieldNames must be equal to the number of newValues" << endl;
+            }
+            if (!fieldNames.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::Update : the fieldNames array must not be empty" << endl;
+            }
+            if (!newValues.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::Update : the newValuess array must not be empty" << endl;
+            }
+
+
+            string sql_com = "";
+
+            unsigned int num_columns = fieldNames.size();
+
+            sql_com += "UPDATE " + tableName + " SET ";
+
+            string h = "";
+            for (unsigned int i = 0 ; i < num_columns ; i++){
+                h += fieldNames[i] + "=" + newValues[i] + ",";
+            }
+            h.pop_back();     //To remove the last comma character
+
+            sql_com += h;
+
+            if (cond[0])    sql_com += " WHERE (" + cond + ")";
+
+            return sql_com + ";";
+        };
+
+        //*cond* here is an sql statement appended after the WHERE keyword, it must not include the semicolon and the main surrounding brackets
+        static string Delete(const string &tableName,const string &cond=""){
+            string sql_com = "";
+
+            sql_com += "DELETE FROM " + tableName;
+
+            if (cond[0])    sql_com += " WHERE (" + cond + ")";
+
+            return sql_com + ";";
+        };
+
+        //*concat* here is an sql statement appended after the generated SELECT statement, it may not include the semicolon
+        static string SelectFromOneTable(const string &tableName,const vector<string> &fieldNames,const string &concat=""){
+            if (!fieldNames.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::SelectFromOneTable : the fieldNames array must not be empty" << endl;
+            }
+
+            string sql_com = "";
+
+            unsigned int num_columns = fieldNames.size();
+
+            sql_com += "SELECT ";
+
+            string h = "";
+            for (unsigned int i = 0 ; i < num_columns ; i++){
+                h += fieldNames[i]+",";
+            }
+            h.pop_back();      //Remove last comma character
+
+            sql_com += h + " FROM " + tableName;
+
+            if (concat[0])    sql_com += " " + concat;
+
+            return sql_com + ";";
+        };
+
+        ////*concat* here is an sql statement appended after the generated SELECT statement, it may not include the semicolon
+        static string SelectAllFromOneTable(const string &tableName,const string &concat=""){
+            return SelectFromOneTable(tableName,{"*"},concat);
+        };
+
+        //*concat* here is an sql statement appended after the generated SELECT statement, it may not include the semicolon
+        static string SelectFromManyTables(const vector<string> &tableNames,const vector<vector<string>> &fieldNamesArrays,const string &concat=""){
+            if (fieldNamesArrays.size() != tableNames.size()){
+                cerr << "*Warning* function WeakSQLCommandGenerator::SelectFromManyTables : the number of tableNames must be equal to the number of fieldNamesArrays" << endl;
+            }
+            if (!tableNames.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::SelectFromManyTables : the tableNames array must not be empty" << endl;
+            }
+            if (!fieldNamesArrays.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::SelectFromManyTables : the fieldNamesArrays array must not be empty" << endl;
+            }
+
+
+            string sql_com = "";
+
+            sql_com += "SELECT ";
+
+            //Field Names in format {TableName}.{FieldName}
+            string h = "";
+            for (unsigned int i = 0 ; i < tableNames.size() ; i++){
+                for (unsigned int j = 0 ; j < fieldNamesArrays[i].size() ; j++){
+                    h += tableNames[i] + "." + fieldNamesArrays[i][j] + ",";
+                }
+            }
+            h.pop_back();      //Remove last comma character
+
+            sql_com += h + " FROM ";
+
+            //Table Names
+            string g = "";
+            for (unsigned int i = 0 ; i < tableNames.size() ; i++){
+                g += tableNames[i] + ",";
+            }
+            g.pop_back();      //Remove last comma character
+
+            sql_com += g;
+
+            if (concat[0])    sql_com += " " + concat;
+
+            return sql_com + ";";
+        };
+
+        //*concat* here is an sql statement appended after the generated SELECT statement, it may not include the semicolon
+        static string SelectAllFromManyTables(const vector<string> &tableNames,const string &concat=""){
+            if (!tableNames.size()){
+                cerr << "*Fatal* function WeakSQLCommandGenerator::SelectAllFromManyTables : the tableNames array must not be empty" << endl;
+            }
+
+
+            string sql_com = "";
+
+            sql_com += "SELECT * FROM ";
+
+            //Table Names
+            string g = "";
+            for (unsigned int i = 0 ; i < tableNames.size() ; i++){
+                g += tableNames[i] + ",";
+            }
+            g.pop_back();      //Remove last comma character
+
+            sql_com += g;
+
+            if (concat[0])    sql_com += " " + concat;
+
+            return sql_com + ";";
+        };
+
+};
+
+
+#endif // WEAKSQLCOMMANDGENERATOR_H_INCLUDED
