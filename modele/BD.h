35437e153dcff4860f0b74b68746021bfda32d09
bemmo <bemmoFulbert@gmail.com>
2022-07-10 22:55:34 +0200

    first

---
+++ modele/BD.h
@@ -0,0 +1,18 @@
+#ifndef BD_H
+#define BD_H
+
+#include <string>
+
+using namespace std;
+
+#include <QtSql/QSqlDatabase>
+
+#define BD_NAME "dut_puc2442_proj.db"
+
+class BD: public QSqlDatabase{
+    public:
+        BD(QString data_base_type);
+        ~BD();
+};
+#endif // BD_H
+
