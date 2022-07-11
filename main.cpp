4270e10df8502b14cf9dfee9fe972af8b782a09a
bemmo <bemmoFulbert@gmail.com>
2022-07-11 15:13:25 +0200

    new

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QToolBar>
 
-#include "modele/BD.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    BD bdd("QSQLITE");
-    w.addToolBar(new QToolBar(&w));
     w.show();
     return a.exec();
 }
